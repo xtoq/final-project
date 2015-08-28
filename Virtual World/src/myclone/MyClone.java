4b58d85733f360ce9a0bb60dc6461792a8a08738
Smokie Lee <xtoq@users.noreply.github.com>
2015-08-27 20:34:45 -0500

    0.1.3.1
    - didn't go with the other commit for some reason?

--- Virtual World/src/myclone/MyClone.java
+++ Virtual World/src/myclone/MyClone.java
@@ -9,6 +9,31 @@ package myclone;
  *
  * @author Smokie Lee
  */
-public class myClone {
+public class MyClone {
+  
+  //  introduction method
+  //  - welcome user
+  //  - get first name from user
+  //  - get last name from user
+  //  - explain what's going on
+  //  - print name of avatar
+  
+  public MyClone() {
+//    this(null);
+  }
+  
+  public void introduction() {
+    // intro method
+//    userInput u = new userInput();
+    String printIntro = "";
+    System.out.printf("\tWelcome to the \"Virtual World\" program!%n\t\tCreated by Smokie Lee");
+    System.out.printf("%n\t---------------------------------------%n");
+    System.out.printf("");
+//    return printIntro;
+  }
+  
+//  public void setIntroduction() {
+//    // setter
+//  }
   
 }
