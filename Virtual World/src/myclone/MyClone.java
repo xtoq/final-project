126e3da5fb58b0d1214b9124b445bb0e6c0182c5
Smokie Lee <xtoq@users.noreply.github.com>
2015-08-27 20:02:13 -0500

    0.1b
    - renamed main class to VirtualWorld
    - added "new" myClone class

--- Virtual World/src/myclone/MyClone.java
+++ Virtual World/src/myclone/MyClone.java
@@ -6,22 +6,9 @@
 package myclone;
 
 /**
+ *
  * @author Smokie Lee
  */
-public class MyClone {
-  
-//  introduction method
-//  - welcome user
-//  - get first name from user
-//  - get last name from user
-//  - explain what's going on
-//  - print name of avatar
-  
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    
-  }
+public class myClone {
   
 }
---
+++ Virtual World/src/myclone/VirtualWorld.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclone;
+
+/**
+ * @author Smokie Lee
+ */
+public class VirtualWorld {
+  
+//  introduction method
+//  - welcome user
+//  - get first name from user
+//  - get last name from user
+//  - explain what's going on
+//  - print name of avatar
+  
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    
+  }
+  
+}
